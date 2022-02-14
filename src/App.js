e5097218ea68e0706f7314705136690de11aa3a2
BatyrArtem <artembatyr88@gmail.com>
2022-02-14 14:43:14 +0200

    add button and generator

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React from 'react';
 import Die from './components/Die'
 
 function App() {
-  return (
-    <main>
-      <div className='dice--container'>
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>        
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>  
-        <Die value={1}/>
-        <Die value={1}/>
-        <Die value={1}/>  
-        <Die value={1}/>  
-      </div>
-    </main>
-  );
+      const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+      setDice(allNewDice())
+    }
+
+    const diceElements = dice.map(die => <Die value={die} />)
+    
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className='roll-dice' onClick={rollDice}>Roll</button>
+        </main>
+    )
 }
 
 export default App;
