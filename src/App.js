9ab459f4f102c8aee29fa080ff8a203e330f014e
BatyrArtem <artembatyr88@gmail.com>
2022-02-14 23:40:53 +0200

    add state to digits

--- src/App.js
+++ src/App.js
@@ -2,27 +2,40 @@ import React from 'react';
 import Die from './components/Die'
 import {nanoid} from 'nanoid'
 function App() {
-      const [dice, setDice] = React.useState(allNewDice())
+    const [dice, setDice] = React.useState(allNewDice())
     
+    function generateNewDie() {
+      return {
+          value: Math.ceil(Math.random() * 6),
+          isHeld: false,
+          id: nanoid()
+      }
+    }
+
     function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push({
-              value: Math.ceil(Math.random() * 6), 
-              isHeld: false, 
-              id: nanoid()
-            })
+            newDice.push(generateNewDie())
         }
         return newDice
     }
     
     function rollDice() {
-      setDice(allNewDice())
+      setDice(prevDice => 
+        prevDice.map(dice => 
+          dice.isHeld ? 
+            dice : 
+            generateNewDie()
+        )
+      )
     }
+
     function holdDice(id) {
       setDice(prevDice =>
         prevDice.map(dice =>
-          dice.id === id ? {...dice, isHeld: !dice.isHeld} : dice
+          dice.id === id ? 
+            {...dice, isHeld: !dice.isHeld} : 
+            dice
         )
       )
     }
@@ -38,6 +51,8 @@ function App() {
     
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
