3732cdbba046869349c0cb12abd33858420d5917
BatyrArtem <artembatyr88@gmail.com>
2022-02-15 16:09:07 +0200

    mvp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Die from './components/Die'
 import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
+
+
 function App() {
     const [dice, setDice] = React.useState(allNewDice())
     
+    const [tenzies, setTenzies] = React.useState(false)
+    
+    React.useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld)
+        const firstValue = dice[0].value
+        const allSameValue = dice.every(die => die.value === firstValue)
+        if (allHeld && allSameValue) {
+            setTenzies(true)
+            console.log("You won!")
+
+        }
+    }, [dice])
+
     function generateNewDie() {
       return {
           value: Math.ceil(Math.random() * 6),
@@ -21,13 +37,20 @@ function App() {
     }
     
     function rollDice() {
-      setDice(prevDice => 
-        prevDice.map(dice => 
-          dice.isHeld ? 
-            dice : 
-            generateNewDie()
+
+      if(tenzies) {
+        setDice(allNewDice())
+        setTenzies(false)
+      }
+      else {
+        setDice(prevDice => 
+          prevDice.map(dice => 
+            dice.isHeld ? 
+              dice : 
+              generateNewDie()
+          )
         )
-      )
+      }
     }
 
     function holdDice(id) {
@@ -48,15 +71,17 @@ function App() {
         holdDice={holdDice}
       />
     ))
-    
     return (
         <main>
+            {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className='roll-dice' onClick={rollDice}>Roll</button>
+            <button className='roll-dice' onClick={rollDice}>
+              {tenzies ? "New Game" : "Roll"}
+            </button>
         </main>
     )
 }
